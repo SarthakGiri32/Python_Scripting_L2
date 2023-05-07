5c634017f69a328728eaac5560b2c1241ec86b82
Sarthak Giri <giri.sarthak1@gmail.com>
2023-05-07 13:30:45 +0530

    Adding code for writing to a file

--- Python_Scripting/File_Operations/FileOperations.py
+++ Python_Scripting/File_Operations/FileOperations.py
@@ -1,10 +1,19 @@
+# opening and reading from a file, then closing the file
 try:
     file1 = open("Python_Scripting\\File_Operations\\test.txt")
     readContent = file1.read()
-    print(readContent)
+    print(f"Reading contents from \"test.txt\":\n{readContent}")
 finally:
     file1.close()
 
 with open("Python_Scripting\\File_Operations\\test.txt", "r") as fHandle:
     readCon = fHandle.read()
-    print(readCon)
+    print(f"Reading contents from \"test.txt\":\n{readCon}")
+
+# writing to a file
+with open("Python_Scripting\\File_Operations\\test2.txt", "w") as fwHandle:
+
+    # write contents to the test2.txt file
+    fwHandle.write("Progamming is fun, especially with file operations.")
+    fwHandle.write("\nLets see how far we can go today.")
+
