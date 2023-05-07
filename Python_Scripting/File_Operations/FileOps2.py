dcf23da1e1b1b433bbe3b785c0f22df7105bb480
Sarthak Giri <giri.sarthak1@gmail.com>
2023-05-07 13:47:45 +0530

    Adding code for continuous reading of partial file

---
+++ Python_Scripting/File_Operations/FileOps2.py
@@ -0,0 +1,8 @@
+while True:
+    # reading partial file
+    with open("Python_Scripting\\File_Operations\\test2.txt") as frHandle:
+        numBytes = int(input(f"Enter the number of bytes you want to read from {frHandle.name}:\n"))
+        readPartialContent = frHandle.read(numBytes)
+        print(f"Reading {numBytes} bytes from file \"{frHandle.name}\":\n{readPartialContent}")
+    if input("Enter No to stop:\n") == "No":
+        break
